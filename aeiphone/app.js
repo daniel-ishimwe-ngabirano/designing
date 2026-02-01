4ea08d63df5de04e3782d9465da936725f4697df
daniel5v <ishimwengabiranodaniel916@gmail.com>
2026-02-01 17:40:02 +0200

    initial commit

--- aeiphone/app.js
+++ aeiphone/app.js
@@ -1,43 +1,46 @@
-let nextButton = document.getElementById('next');
-let prevButton = document.getElementById('prev');
-let backButton = document.getElementById('back');
-let seeMoreButtons = document.querySelectorAll('.seeMore');
-let carousel = document.querySelector('.carousel');
-let listHTML = document.querySelector('.carousel .list');
+ let nextButton = document.getElementById('next');
+        let prevButton = document.getElementById('prev');
+        let backButton = document.getElementById('back');
+        let seeMoreButtons = document.querySelectorAll('.seeMore');
+        let carousel = document.querySelector('.carousel');
+        let listHTML = document.querySelector('.carousel .list');
 
-nextButton.onclick = function(){
-    showSlider('next');
-}
-prevButton.onclick = function(){
-    showSlider('prev');
-}
-let unAcceptClick;
-const showSlider = (type) => {
-    nextButton.style.pointerEvents = 'none';
-    prevButton.style.pointerEvents = 'none';
+        nextButton.onclick = function(){
+            showSlider('next');
+        }
+        prevButton.onclick = function(){
+            showSlider('prev');
+        }
 
-    carousel.classList.remove('prev', 'next' );
-    let items = document.querySelectorAll('.carousel .list .item');
-    if(type === 'next') {
-        listHTML.appendChild(items[0]);
-        carousel.classList.add('next');
-    }else{
-        let positionLast = items.length -1;
-        listHTML.prepend(items[positionLast]);
-        carousel.classList.add('prev');
-    }
+        let unAcceptClick;
+        const showSlider = (type) => {
+            nextButton.style.pointerEvents = 'none';
+            prevButton.style.pointerEvents = 'none';
 
-    clearTimeout(unAcceptClick);
-    unAcceptClick = setTimeout(() => {
-        nextButton.style.pointerEvents = 'auto';
-        prevButton.style.pointerEvents = 'auto';
-    }, 2000);
-    }
-    seeMoreButtons.forEach(button => {
-    button.onclick = function(){
-        carousel.classList.add('showDetail');
-    }
-    })
-    backbutton.onclick = function(){
-    carousel.classList.remove('showDetail');
-}
+            carousel.classList.remove('prev', 'next' );
+            let items = document.querySelectorAll('.carousel .list .item');
+            if(type === 'next') {
+                listHTML.appendChild(items[0]);
+                carousel.classList.add('next');
+            }else{
+                let positionLast = items.length - 1;
+                listHTML.prepend(items[positionLast]);
+                carousel.classList.add('prev');
+            }
+
+            clearTimeout(unAcceptClick);
+            unAcceptClick = setTimeout(() => {
+                nextButton.style.pointerEvents = 'auto';
+                prevButton.style.pointerEvents = 'auto';
+            }, 1000); 
+        }
+
+        seeMoreButtons.forEach(button => {
+            button.onclick = function(){
+                carousel.classList.add('showDetail');
+            }
+        });
+
+        backButton.onclick = function(){
+            carousel.classList.remove('showDetail');
+        }
